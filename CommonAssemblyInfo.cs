af7bd824d882b81c56b50495eb02a7fdffc5f8ea
Simon Cropp <simon.cropp@gmail.com>
2013-05-17 14:50:47 +1000

    xamarin nuget support

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Obsolete")]
 [assembly: AssemblyProduct("Obsolete")]
-[assembly: AssemblyVersion("1.6.1.1")]
-[assembly: AssemblyFileVersion("1.6.1.1")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2.0")]
