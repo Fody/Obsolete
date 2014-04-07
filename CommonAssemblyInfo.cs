fe3db2d1b80ecf2210a9f054149ab71a5a87d068
Simon Cropp <simon.cropp@gmail.com>
2014-04-07 20:07:31 +1000

    support new phone libraries

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Obsolete")]
 [assembly: AssemblyProduct("Obsolete")]
-[assembly: AssemblyVersion("3.0.4")]
-[assembly: AssemblyFileVersion("3.0.4")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
