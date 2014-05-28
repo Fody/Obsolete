c3222a9348dbd75f43d0f1c67efc007ed781f6ed
Simon Cropp <simon.cropp@gmail.com>
2014-05-28 15:10:44 +1000

    better pcl support

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Obsolete")]
 [assembly: AssemblyProduct("Obsolete")]
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
