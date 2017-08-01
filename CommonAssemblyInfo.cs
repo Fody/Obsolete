81b7b064afddfb869f30c1c338a6f19e7c2c7ec6
Simon Cropp <simon.cropp@gmail.com>
2017-08-01 11:01:41 +1000

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Obsolete")]
 [assembly: AssemblyProduct("Obsolete")]
-[assembly: AssemblyVersion("4.2.2")]
-[assembly: AssemblyFileVersion("4.2.2")]
+[assembly: AssemblyVersion("4.2.3")]
+[assembly: AssemblyFileVersion("4.2.3")]
