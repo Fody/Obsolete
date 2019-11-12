08ba27269a3d0ab245b0317b62ae131914020efb
Simon Cropp <simon.cropp@gmail.com>
2019-11-12 17:55:40 +1100

    missing project dependency

--- AssemblyToProcess/ClassToMark.cs
+++ AssemblyToProcess/ClassToMark.cs
@@ -4,39 +4,70 @@ using System;
 #pragma warning disable 67
 #pragma warning disable 649
 
-[ObsoleteEx(TreatAsErrorFromVersion = "2.0", RemoveInVersion = "4.0", Message = "Custom message.", ReplacementTypeOrMember = "NewThing")]
+[ObsoleteEx(
+    TreatAsErrorFromVersion = "2.0",
+    RemoveInVersion = "4.0",
+    Message = "Custom message.",
+    ReplacementTypeOrMember = "NewThing")]
 public class ClassToMark
 {
-    [ObsoleteEx(TreatAsErrorFromVersion = "2.0", RemoveInVersion = "4.0", Message = "Custom message.", ReplacementTypeOrMember = "NewThing")]
+    [ObsoleteEx(
+        TreatAsErrorFromVersion = "2.0",
+        RemoveInVersion = "4.0",
+        Message = "Custom message.",
+        ReplacementTypeOrMember = "NewThing")]
     public string PropertyToMark { get; set; }
 
-    [ObsoleteEx(TreatAsErrorFromVersion = "2.0", RemoveInVersion = "4.0", Message = "Custom message.", ReplacementTypeOrMember = "NewThing")]
+    [ObsoleteEx(
+        TreatAsErrorFromVersion = "2.0",
+        RemoveInVersion = "4.0",
+        Message = "Custom message.",
+        ReplacementTypeOrMember = "NewThing")]
     public string PropertyWithSetExceptionToMark
     {
         get => propertyWithSetExceptionToMark;
         set => throw new NotImplementedException();
     }
 
-    [ObsoleteEx(TreatAsErrorFromVersion = "2.0", RemoveInVersion = "4.0", Message = "Custom message.", ReplacementTypeOrMember = "NewThing")]
+    [ObsoleteEx(
+        TreatAsErrorFromVersion = "2.0",
+        RemoveInVersion = "4.0",
+        Message = "Custom message.",
+        ReplacementTypeOrMember = "NewThing")]
     public string PropertyWithGetExceptionToMark
     {
         get => throw new NotImplementedException();
         set => propertyWithGetExceptionToMark = value;
     }
 
-    [ObsoleteEx(TreatAsErrorFromVersion = "2.0", RemoveInVersion = "4.0", Message = "Custom message.", ReplacementTypeOrMember = "NewThing")]
+    [ObsoleteEx(
+        TreatAsErrorFromVersion = "2.0",
+        RemoveInVersion = "4.0",
+        Message = "Custom message.",
+        ReplacementTypeOrMember = "NewThing")]
     public void MethodToMark() { }
 
-    [ObsoleteEx(TreatAsErrorFromVersion = "2.0", RemoveInVersion = "4.0", Message = "Custom message.", ReplacementTypeOrMember = "NewThing")]
+    [ObsoleteEx(TreatAsErrorFromVersion = "2.0",
+        RemoveInVersion = "4.0",
+        Message = "Custom message.",
+        ReplacementTypeOrMember = "NewThing")]
     public void MethodWithExceptionToMark()
     {
         throw new NotImplementedException();
     }
 
-    [ObsoleteEx(TreatAsErrorFromVersion = "2.0", RemoveInVersion = "4.0", Message = "Custom message.", ReplacementTypeOrMember = "NewThing")]
+    [ObsoleteEx(
+        TreatAsErrorFromVersion = "2.0",
+        RemoveInVersion = "4.0",
+        Message = "Custom message.",
+        ReplacementTypeOrMember = "NewThing")]
     public event EventHandler EventToMark;
 
-    [ObsoleteEx(TreatAsErrorFromVersion = "2.0", RemoveInVersion = "4.0", Message = "Custom message.", ReplacementTypeOrMember = "NewThing")]
+    [ObsoleteEx(
+        TreatAsErrorFromVersion = "2.0",
+        RemoveInVersion = "4.0",
+        Message = "Custom message.",
+        ReplacementTypeOrMember = "NewThing")]
     public string FieldToMark;
 
     string propertyWithGetExceptionToMark;
--- AssemblyToProcess/ClassWithIsError.cs
+++ AssemblyToProcess/ClassWithIsError.cs
@@ -1,4 +1,6 @@
-[ObsoleteEx(TreatAsErrorFromVersion = "0.5",RemoveInVersion = "1.1")]
+[ObsoleteEx(
+    TreatAsErrorFromVersion = "0.5",
+    RemoveInVersion = "1.1")]
 public class ClassWithIsError
 {
 }
