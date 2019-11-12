deb74f5c7ade2ff476ca13e88b659e51b27168fb
Simon Cropp <simon.cropp@gmail.com>
2019-11-12 18:37:03 +1100

    Update DataReader.cs

--- Obsolete.Fody/DataReader.cs
+++ Obsolete.Fody/DataReader.cs
@@ -2,17 +2,14 @@ using Mono.Cecil;
 
 public static class DataReader
 {
-    public static AttributeData ReadAttributeData(CustomAttribute obsoleteExAttribute, bool throwsNotImplemented)
+    public static AttributeData ReadAttributeData(CustomAttribute attribute, bool throwsNotImplemented)
     {
-        var treatAsErrorFromVersionString = obsoleteExAttribute.GetValue("TreatAsErrorFromVersion");
-        var removeInVersionString = obsoleteExAttribute.GetValue("RemoveInVersion");
-
         return new AttributeData
         {
-            Message = obsoleteExAttribute.GetValue("Message"),
-            Replacement = obsoleteExAttribute.GetValue("ReplacementTypeOrMember"),
-            TreatAsErrorFromVersion = treatAsErrorFromVersionString,
-            RemoveInVersion = removeInVersionString,
+            Message = attribute.GetValue("Message"),
+            Replacement = attribute.GetValue("ReplacementTypeOrMember"),
+            TreatAsErrorFromVersion = attribute.GetValue("TreatAsErrorFromVersion"),
+            RemoveInVersion = attribute.GetValue("RemoveInVersion"),
             ThrowsNotImplemented = throwsNotImplemented
         };
     }
