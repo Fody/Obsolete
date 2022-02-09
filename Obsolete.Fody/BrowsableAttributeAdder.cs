75ba42fa4e538a01a06670bd5b10d30ec0c33f0c
danielmarbach <daniel.marbach@openplace.net>
2022-02-09 11:25:45 +0100

    Support to overload the editor browsable state including more test coverage

--- Obsolete.Fody/BrowsableAttributeAdder.cs
+++ Obsolete.Fody/BrowsableAttributeAdder.cs
@@ -4,13 +4,15 @@ using Mono.Collections.Generic;
 
 public partial class ModuleWeaver
 {
-    public void AddEditorBrowsableAttribute(Collection<CustomAttribute> customAttributes)
+    public void AddEditorBrowsableAttribute(Collection<CustomAttribute> customAttributes, HideObsoleteMembersState state)
     {
-        if (customAttributes.Any(x => x.AttributeType.Name == "EditorBrowsableAttribute"))
+        if (customAttributes.Any(x => x.AttributeType.Name == "EditorBrowsableAttribute") || state == HideObsoleteMembersState.Off)
         {
             return;
         }
-        var customAttribute = new CustomAttribute(EditorBrowsableConstructor);        var customAttributeArgument = new CustomAttributeArgument(EditorBrowsableStateType, AdvancedStateConstant);        customAttribute.ConstructorArguments.Add(customAttributeArgument);
+        var customAttribute = new CustomAttribute(EditorBrowsableConstructor);
+        var customAttributeArgument = new CustomAttributeArgument(EditorBrowsableStateType, state == HideObsoleteMembersState.Advanced ? AdvancedStateConstant : NeverStateConstant);
+        customAttribute.ConstructorArguments.Add(customAttributeArgument);
         customAttributes.Add(customAttribute);
     }
 }
